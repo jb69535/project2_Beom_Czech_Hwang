462f661f2ffc44e4a1d5913dda2541923216278d
Joon Hwag <110270444+whwan4570@users.noreply.github.com>
2024-02-24 21:02:25 -0500

    Update problem1.sql

--- src/main/resources/problem1.sql
+++ src/main/resources/problem1.sql
@@ -1,13 +1,45 @@
--- problem1.sql
-
-SELECT departments.dept_name, 
-       (SUM(IF(employees.gender = 'F', salaries.salary, 0)) / COUNT(IF(employees.gender = 'F', 1, NULL))) / 
-       (SUM(IF(employees.gender = 'M', salaries.salary, 0)) / COUNT(IF(employees.gender = 'M', 1, NULL))) AS ratio
-FROM departments 
-JOIN dept_emp  ON departments.dept_no = dept_emp.dept_no
-JOIN employees ON dept_emp.emp_no = employees.emp_no
-JOIN salaries  ON employees.emp_no = salaries.emp_no
-WHERE dept_emp.to_date > NOW() AND salaries.to_date > NOW()  
-GROUP BY departments.dept_no
-ORDER BY ratio DESC
-LIMIT 100;
+SELECT 
+    d.dept_name, 
+    ROUND(
+        (SUM(IF(e.gender = 'F', s.salary, 0)) / COUNT(IF(e.gender = 'F', 1, NULL))) / 
+        (SUM(IF(e.gender = 'M', s.salary, 0)) / COUNT(IF(e.gender = 'M', 1, NULL))),
+        8
+    ) AS ratio
+FROM 
+    departments d
+JOIN 
+    dept_emp de ON d.dept_no = de.dept_no
+JOIN 
+    employees e ON de.emp_no = e.emp_no
+JOIN 
+    salaries s ON e.emp_no = s.emp_no
+WHERE 
+    de.to_date > NOW() AND s.to_date > NOW()
+GROUP BY 
+    d.dept_no
+HAVING 
+    ratio = (
+        SELECT 
+            MAX(ratio)
+        FROM (
+            SELECT 
+                d.dept_no,
+                ROUND(
+                    (SUM(IF(e.gender = 'F', s.salary, 0)) / COUNT(IF(e.gender = 'F', 1, NULL))) / 
+                    (SUM(IF(e.gender = 'M', s.salary, 0)) / COUNT(IF(e.gender = 'M', 1, NULL))),
+                    8
+                ) AS ratio
+            FROM 
+                departments d
+            JOIN 
+                dept_emp de ON d.dept_no = de.dept_no
+            JOIN 
+                employees e ON de.emp_no = e.emp_no
+            JOIN 
+                salaries s ON e.emp_no = s.emp_no
+            WHERE 
+                de.to_date > NOW() AND s.to_date > NOW()
+            GROUP BY 
+                d.dept_no
+        ) AS subquery
+    );
